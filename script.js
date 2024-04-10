74a424c4dbc8886f0d0df4a7bcf3dda55afbf181
Ezinne Okonkwo <zidede1@icloud.com>
2024-04-09 17:00:31 -0700

    4/9 work

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ document.addEventListener('DOMContentLoaded', function() {
       }, 1000); // Matches the CSS transition time
     }, 3000); // Change text every 3 seconds
   });
+
+// custom header for pages
+class MyHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <div class="pageHeader">
+          <div class="header-buttons"><p>HOME</p></div>
+          <div class="header-buttons"><p>HISTORY</p></div>
+          <div class="header-buttons"><p>EDUCATION</p></div>
+          <div class="header-buttons"><p>FUN</p></div>
+        </div>
+        <div><hr class="nav-bar-line"></div>
+      </header>
+    `;
+  }
+}
+// custom element: if I want to change it, only have to do so in one file
+customElements.define('my-header', MyHeader)
